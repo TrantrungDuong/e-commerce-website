28ff24320074f001b5e6a496f7f2cc7dffcdec5f
TrantrungDuong <duongtran27052003@gmail.com>
2025-06-04 13:30:39 +0700

    login-by-google

---
+++ web-app/src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+  clientId: "304589200642-gbg7qnk2b7mblq7vllcmjt1ekbdi8ebr.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
